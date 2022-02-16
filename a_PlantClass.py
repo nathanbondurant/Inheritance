ae0548c9ea7f92636c8702c43c19affeaa9d34ca
Nathan Bondurant <nathanbondurant_bon2@baylor.edu>
2022-02-16 09:35:23 -0600

    gitignore

--- a_PlantClass.py
+++ a_PlantClass.py
@@ -8,10 +8,10 @@ class Plant:
         return self.__color
 
 
-class Flower(Plant):
+class Flower(Plant): #this is a specialized x of plant- it inherits the attributes from Plant
     def __init__(self,color, petals):
         Plant.__init__(self,color)
-
+        #you can call methods from the supeclass, not the actuall attribute ex. __color
         self.__petals = petals
 
     def get_petals(self):
--- a_PlantProgram.py
+++ a_PlantProgram.py
@@ -2,12 +2,12 @@ import a_PlantClass as pc
 
 primrose = pc.Plant("Green")
 
-sunflower = pc.Flower("Yellow")
+sunflower = pc.Flower("Yellow", 12)
 
 print(primrose.get_color())
 
-print(sunflower.get_color())
+print(sunflower.get_color()) #even though it is a flower, it can call the methods in the super clas (plant)
 print(sunflower.get_petals())
 
 
-print(primrose.get_petals())
+#print(primrose.get_petals()) it cannot do this- method is only in the subclass
